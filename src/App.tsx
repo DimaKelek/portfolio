c665aca90a4209c059e807ace4c360be54d051e5
Dmitry Kelek <kelekofficial@gmail.com>
2021-06-13 13:45:21 +0300

    added contacts form

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Header} from "./components/Header/Header";
 import {Main} from "./components/Main/Main";
 import {Skills} from "./components/Skills/Skills";
 import {Footer} from "./components/Footer/Footer";
+import {ContactForm} from "./components/ContactForm/ContactForm";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Header />
             <Main />
             <Skills />
+            <ContactForm />
             <Footer />
         </div>
     );
---
+++ src/components/ContactForm/ContactForm.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import S from "./ContactForm.module.css"
+import Sc from "./Containers.module.css";
+import {MyButton} from "./MyButton/MyButton";
+import chakres from "../../common/others/chakres.png"
+import iconReact from "../../common/icons/react.svg"
+import iconTS from "../../common/icons/typescript.svg"
+import iconRedux from "../../common/icons/redux.svg"
+import iconHTML from "../../common/icons/html5.svg"
+import iconCSS from "../../common/icons/css3.svg"
+import iconJest from "../../common/icons/jest.png"
+
+export function ContactForm() {
+    return (
+        <div className={Sc.contactForm}>
+            <div className={`${Sc.container} ${S.title}`}>
+                <span>let's talk</span>
+                <h2>Get in Touch</h2>
+            </div>
+            <div className={Sc.form_container}>
+                <div className={Sc.image_container}>
+                    <div className={Sc.chakres_box}>
+                        <img className={S.chakres} src={chakres} alt="image"/>
+                        <div className={`${S.react}`}><img src={iconReact} alt="React"/></div>
+                        <div className={`${S.icon} ${S.ts}`}><img src={iconTS} alt="TS"/></div>
+                        <div className={`${S.icon} ${S.redux}`}><img src={iconRedux} alt="Redux"/></div>
+                        <div className={`${S.icon} ${S.html5}`}><img src={iconHTML} alt="HTML"/></div>
+                        <div className={`${S.icon} ${S.css3}`}><img src={iconCSS} alt="CSS"/></div>
+                        <div className={`${S.icon} ${S.jest}`}><img src={iconJest} alt="Jest"/></div>
+                    </div>
+                </div>
+                <div className={Sc.form_elements_box}>
+                    <div className={Sc.namePLUSemail_container}>
+                        <div className={`${S.form_group} ${S.name}`}>
+                            <input type="text" placeholder="Your name"/>
+                        </div>
+                        <div className={`${S.form_group} ${S.email}`}>
+                            <input type="text" placeholder="Your email"/>
+                        </div>
+                    </div>
+                    <div className={`${S.form_group} ${S.subject}`}>
+                        <input type="text" placeholder="Your subject"/>
+                    </div>
+                    <div className={`${S.form_group} ${S.message}`}>
+                        <textarea placeholder="Your message"/>
+                    </div>
+                    <div className={`${S.form_group} ${S.button}`}>
+                    <MyButton>Send Message</MyButton>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
